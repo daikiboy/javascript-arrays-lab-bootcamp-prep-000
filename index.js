2103c3b7528326c680bee767002bfc42596c86c1
simone.rainieri <simone.rainieri@gmail.com>
2019-01-22 16:19:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens
+};
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens
+};
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(kittens);
+  return kittens
+};
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift(kittens);
+  return kittens
+};
+
+function appendKitten(name) {
+  
+}
